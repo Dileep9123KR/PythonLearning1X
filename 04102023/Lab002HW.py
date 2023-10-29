30cab4ddfb91e052bfc4f896786ee9249da65697
Dileep K R <dileepdees@gmail.com>
2023-10-29 15:47:56 +0530

    04102023 Python Class

---
+++ 04102023/Lab002HW.py
@@ -0,0 +1,19 @@
+# Print the Name of the user by taking it from the input command.
+
+name = input("Please enter your name ")
+print(name)
+
+# Take first name and last name of user and print with sep. = "-" and end with \t
+
+name1 = input("Enter your first name ")
+name2 = input("Enter your last name ")
+# instead end with \t used \n for displaying output in a new line
+print(name1, name2, sep="-", end="\n")
+
+# Take a user input as name and say with message: You are welcome. "<Dileep>" to the Python Class.
+
+name3 = input("Please enter your name ")
+print("You are welcome", name3, "to the Python Class.")
+
+
+
